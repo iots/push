0e364487f2bebbde127499f7cd2097cef37ba677
ohlsj <ohlsj@163.com>
2015-07-30 10:13:55 +0800

    add first test

--- push/app/tests.py
+++ push/app/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.test import Client
+from .models import PushModel
 
-# Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def testPushView(self):
+        response = self.client.post('/push/', {'push_message': 'test push view message', 'push_url': 'http://www.baidu.com'}, follow=True)
+        self.assertTrue(len(response.redirect_chain) > 0)
+        self.assertEqual(response.status_code, 200)
+
+        self.pushmodel = PushModel.objects.get(push_message = 'test push view message')
+        self.assertEqual(self.pushmodel.push_message, 'test push view message')
+        self.assertEqual(self.pushmodel.push_url, 'http://www.baidu.com')
